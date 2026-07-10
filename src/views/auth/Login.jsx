8c2ca0d691a27b808da9f930ef51c5464b01e1ae
devjmmg <166524587+devjmmg@users.noreply.github.com>
2026-07-10 00:18:28 -0600

    feat: add functionality to do login in our app feat: save the token for validate users

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,18 +1,64 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
+import Alert from "../../components/Alert";
+import api from "../../config/axios"
 
 export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [alert, setAlert] = useState({});
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const errors = {};
+
+        if (!email.trim()) {
+            errors.email = 'El correo electrónico es requerido.';
+        }
+
+        if (!password.trim()) {
+            errors.password = 'La contraseña es requerida.';
+        }
+
+        setAlert(errors);
+
+        if (Object.keys(errors).length > 0) {
+            return;
+        }
+
+        try {
+            const { data: {token} } = await api.post('/login', {
+                email,
+                password
+            });
+            localStorage.setItem('token', token);
+        } catch (error) {
+            if (error.response) {
+                setAlert(error.response.data);
+            } else {
+                setAlert({ message: "No fue posible conectar con el servidor.", type: 'error' });
+            }
+        }
+    }
+
     return (
         <div className="w-full md:max-w-xl mx-auto">
-            <h1 className="text-4xl font-extrabold text-center">Administrador de <span className="text-amber-500">veterinaria</span></h1>
-            <p className="text-gray-500 mt-1 text-center">Ingresa tus credenciales para iniciar sesión.</p>
-            <form action="" className="mt-12 space-y-4">
+            <h1 className="text-4xl font-extrabold text-center mb-1">Administrador de <span className="text-amber-500">veterinaria</span></h1>
+            <p className="text-gray-500 mb-12 text-center">Ingresa tus credenciales para iniciar sesión.</p>
+
+            {alert.error && <Alert message={alert.error} type="error" className="mb-2 text-center" />}
+
+            <form onSubmit={handleSubmit} className="space-y-4">
                 <div>
                     <label className="block" htmlFor="email">Correo electrónico</label>
-                    <input type="email" id="email" name="email" className="bg-gray-100 w-full p-2 rounded-md border-amber-500 border focus:outline-amber-500" />
+                    <input type="email" id="email" name="email" className="bg-gray-100 w-full p-2 rounded-md border-amber-500 border focus:outline-amber-500" value={email} onChange={ e => setEmail(e.target.value)} />
+                    {alert.email && <Alert message={alert.email} type="error" />}
                 </div>
                 <div>
                     <label className="block" htmlFor="password">Contraseña</label>
-                    <input type="password" id="password" name="password" className="bg-gray-100 w-full p-2 rounded-md border-amber-500 border focus:outline-amber-500" />
+                    <input type="password" id="password" name="password" className="bg-gray-100 w-full p-2 rounded-md border-amber-500 border focus:outline-amber-500" value={password} onChange={ e => setPassword(e.target.value)} />
+                    {alert.password && <Alert message={alert.password} type="error" />}
                 </div>
                 <div className="flex justify-end items-center">
                     <input type="submit" value="Iniciar sesión" className="w-full sm:w-auto bg-amber-500 hover:bg-amber-600 transition ease-linear duration-300 p-2 text-white font-medium rounded cursor-pointer"/>

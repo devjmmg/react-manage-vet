6b2b06b12dd056f7d6c827c5a684082359c4bd3f
devjmmg <166524587+devjmmg@users.noreply.github.com>
2026-07-15 04:46:24 -0600

    feat: change the title to a link with logo feat: delete the code line that it already isn't required (import React from 'react')

--- src/components/admin/Header.jsx
+++ src/components/admin/Header.jsx
@@ -11,10 +11,9 @@ export default function Header() {
         <header className="bg-indigo-500 p-4">
             <div className="max-w-5xl mx-auto flex flex-col md:flex-row justify-between items-center">
 
-                <h1 className="text-white font-semibold mb-4 md:m-0">
-                    Administrador de veterinaria
-                </h1>
-
+                <Link to='/'>
+                    <img src="/public/img/logo.svg" alt="Logo" className="w-10 h-10" />
+                </Link>
                 <nav className="flex flex-col md:flex-row items-center gap-3 text-white">
                     <Link to="/">Dashboard</Link>
                     <Link to="/pets">Mascotas</Link>
@@ -30,7 +29,7 @@ export default function Header() {
                         </button>
                         {open && (
                             <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg overflow-hidden">
-                                <Link to="/profile" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                                <Link to="/profile" className="block px-4 py-2 text-gray-700 hover:bg-gray-100" onClick={() => setOpen(!open)}>
                                     Perfil
                                 </Link>
                                 <button onClick={logout} className="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100">
--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,8 +1,6 @@
 import { useContext } from "react";
 import AuthContext from "../context/AuthProvider";
 
-import React from 'react'
-
 export default function useAuth() {
     return useContext(AuthContext);
 }
